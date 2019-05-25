e5016c2fcc1128a0f4f6ddd25e47a49376e75a95
Oliver Davies <oliver@oliverdavies.uk>
2019-05-25 01:04:57 +0100

    Display sessions from Drupal

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="app" class="min-h-screen font-sans bg-white text-black">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="antialiased min-h-screen font-sans bg-gray-100 text-black p-12">
+    <div class="max-w-5xl mx-auto">
+      <h1 class="text-4xl font-semibold mb-2">Sessions</h1>
+
+      <div v-if="sessions" class="bg-white p-6 rounded-lg border">
+        <ul class="-mb-3">
+          <li v-for="session in sessions" :key="session.attributes.drupal_internal__nid" class="mb-3">
+            {{ session.attributes.title }}
+          </li>
+        </ul>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
@@ -12,4 +18,21 @@
 
 <script>
 const axios = require('axios')
+
+module.exports = {
+  data () {
+    return {
+      sessions: []
+    }
+  },
+
+  created () {
+    const baseUrl = 'http://drupaltestcamp.docksal'
+
+    axios.get(`${baseUrl}/jsonapi/node/session`)
+      .then(({ data }) => {
+        this.sessions = data.data
+      })
+  }
+}
 </script>
