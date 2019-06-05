e385fa570f9d1aa307f1ce2156be55172dd46e51
Oliver Davies <oliver@oliverdavies.uk>
2019-06-05 21:40:05 +0100

    Change closing tag for consistency

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" class="antialiased min-h-screen font-sans bg-gray-100 text-black p-12">
     <div class="w-full max-w-2xl mx-auto">
       <accepted-sessions-list :sessions="sortedSessions" />
-      <session-form @submitted="addSession($event)"></session-form>
+      <session-form @submitted="addSession($event)" />
     </div>
   </div>
 </template>
