6ce41c0f0b0ee25979581522ec941ab4c4b5e0ff
Oliver Davies <oliver@oliverdavies.uk>
2019-08-30 17:57:30 +0100

    Add and configure PurgeCSS

--- vuejs/postcss.config.js
+++ vuejs/postcss.config.js
@@ -3,6 +3,13 @@ module.exports = {
     require('postcss-import'),
     require('tailwindcss'),
     require('postcss-nested'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+      content: [
+        './src/**/*.vue',
+        './public/index.html',
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
   ]
 }
